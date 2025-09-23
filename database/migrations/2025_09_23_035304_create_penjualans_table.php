4fc2d0b86737838261e56de7eca7d0d3bdc1f370
sandiarbaa <sandiarbaa@gmail.com>
2025-09-23 10:58:54 +0700

    add model & schema db

---
+++ database/migrations/2025_09_23_035304_create_penjualans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penjualans', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_nota')->unique();
+            $table->date('tgl');
+            $table->foreignId('pelanggan_id')->constrained('pelanggans')->onDelete('cascade');
+            $table->decimal('subtotal', 15, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penjualans');
+    }
+};
